2026f0bfbed0aa4c2615593c2c62c4bd9a0e16d6
Lang Hames <lhames@gmail.com>
2025-08-17 15:00:05 +1000

    Fixes check-orc-rt in bootstrap mode.

--- orc-rt/cmake/OrcRTTesting.cmake
+++ orc-rt/cmake/OrcRTTesting.cmake
@@ -1,39 +1,36 @@
 # Keep track if we have all dependencies.
 set(ORC_RT_LLVM_TOOLS_AVAILABLE TRUE)
 
-# Find executables.
-if (TARGET utils/llvm-lit/all)
-  list(APPEND utils/llvm-lit/all)
-else()
-  find_program(ORC_RT_LLVM_LIT_EXECUTABLE
-    NAMES llvm-lit.py llvm-lit lit.py lit
-    PATHS ${ORC_RT_LLVM_TOOLS_DIR})
-  if (NOT ORC_RT_LLVM_LIT_EXECUTABLE)
-    message(STATUS "Cannot find llvm-lit. Please put it in your PATH, set ORC_RT_LLVM_LIT_EXECUTABLE to its full path, or point ORC_RT_LLVM_TOOLS_DIR to its directory.")
-    set(ORC_RT_LLVM_TOOLS_AVAILABLE FALSE)
-  endif()
+if (NOT DEFINED ORC_RT_LLVM_TOOLS_DIR AND DEFINED LLVM_BINARY_DIR)
+  cmake_path(APPEND ORC_RT_LLVM_TOOLS_DIR "${LLVM_BINARY_DIR}" "bin")
 endif()
 
+if (TARGET utils/llvm-lit/all)
+   list(APPEND utils/llvm-lit/all)
+endif()
+
+# Add dependence on FileCheck.
 if (TARGET FileCheck)
   list(APPEND ORC_RT_TEST_DEPS FileCheck)
-else()
-  find_program(ORC_RT_FILECHECK_EXECUTABLE
-    NAMES FileCheck
-    PATHS ${ORC_RT_LLVM_TOOLS_DIR})
-  if (NOT ORC_RT_FILECHECK_EXECUTABLE)
-    message(STATUS "Cannot find FileCheck. Please put it in your PATH, set ORC_RT_FILECHECK_EXECUTABLE to its full path, or point ORC_RT_LLVM_TOOLS_DIR to its directory.")
-    set(ORC_RT_LLVM_TOOLS_AVAILABLE FALSE)
-  endif()
 endif()
 
+find_program(ORC_RT_FILECHECK_EXECUTABLE
+  NAMES FileCheck
+  PATHS ${ORC_RT_LLVM_TOOLS_DIR})
+if (NOT ORC_RT_FILECHECK_EXECUTABLE)
+  message(STATUS "Cannot find FileCheck. Please put it in your PATH, set ORC_RT_FILECHECK_EXECUTABLE to its full path, or point ORC_RT_LLVM_TOOLS_DIR to its directory.")
+  set(ORC_RT_LLVM_TOOLS_AVAILABLE FALSE)
+endif()
+
+# Add dependence on not.
 if (TARGET not)
   list(APPEND ORC_RT_TEST_DEPS not)
-else()
-  find_program(ORC_RT_NOT_EXECUTABLE
-    NAMES not
-    PATHS ${ORC_RT_LLVM_TOOLS_DIR})
-  if (NOT ORC_RT_NOT_EXECUTABLE)
-    message(STATUS "Cannot find 'not'. Please put it in your PATH, set ORC_RT_NOT_EXECUTABLE to its full path, or point ORC_RT_LLVM_TOOLS_DIR to its directory.")
-    set(ORC_RT_LLVM_TOOLS_AVAILABLE FALSE)
-  endif()
+endif()
+
+find_program(ORC_RT_NOT_EXECUTABLE
+  NAMES not
+  PATHS ${ORC_RT_LLVM_TOOLS_DIR})
+if (NOT ORC_RT_NOT_EXECUTABLE)
+  message(STATUS "Cannot find 'not'. Please put it in your PATH, set ORC_RT_NOT_EXECUTABLE to its full path, or point ORC_RT_LLVM_TOOLS_DIR to its directory.")
+  set(ORC_RT_LLVM_TOOLS_AVAILABLE FALSE)
 endif()
